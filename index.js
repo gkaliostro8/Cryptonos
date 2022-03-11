e3cb2229fa142cea11ce0ec167d44f1b1ce838b1
gkaliostro8 <101426459+gkaliostro8@users.noreply.github.com>
2022-03-11 22:25:26 +0300

    Update index.js
    test

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-/*! standard. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
-import { StandardEngine } from 'standard-engine'
-import options from './options.js'
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import eslint from 'eslint'
 
-export default new StandardEngine(options)
+// eslintConfig.overrideConfigFile have problem reading URLs and file:///
+const overrideConfigFile = fileURLToPath(new URL('./eslintrc.json', import.meta.url))
+const pkgURL = new URL('./package.json', import.meta.url)
+const pkgJSON = readFileSync(pkgURL, { encoding: 'utf-8' })
+const pkg = JSON.parse(pkgJSON)
+
+export default {
+  bugs: pkg.bugs.url,
+  cmd: 'standard',
+  eslint,
+  eslintConfig: {
+    overrideConfigFile
+  },
+  homepage: pkg.homepage,
+  tagline: 'Use JavaScript Standard Style',
+  version: pkg.version
+}
